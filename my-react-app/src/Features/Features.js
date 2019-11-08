59f2f0c059646c38e56f0f28e0ef78e01bfc44e8
Peshakof <nfs2000@abv.bg>
2019-11-08 20:05:31 +0200

    Created initial components

---
+++ my-react-app/src/Features/Features.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../variables.css';
+import './Features.css';
+
+function Features() {
+    return (
+        <div className="Features">
+            <section class="sec1">
+
+            <header>
+                <h1>Personal finance application that makes money management easy</h1>
+            </header>
+
+            <section class="sec2">
+                <h2>Features:</h2>
+                <ul>
+                    <li>
+                        <p> Add your cash expenses manually in a few seconds</p>
+                    </li>
+                    <li>
+                        <p>Easily track where all your money goes at the end of the month</p>
+                    </li>
+                    <li>
+                        <p>Ultimate bill tracker, never forget to pay for gas, water, loan etc</p>
+                    </li>
+                    <li>
+                        <p>Check Account balances</p>
+                    </li>
+                    <li>
+                        <p></p>
+                    </li>
+                </ul>
+            </section>
+            </section>
+            
+        </div>    
+    )
+}
+
+export default Features;
