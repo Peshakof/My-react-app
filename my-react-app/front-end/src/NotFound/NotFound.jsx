b0cb76bd5a0028c3a4bd0a66bb5f7b66088ff2b1
Peshakof <nfs2000@abv.bg>
2019-11-17 11:16:43 +0200

    Added NotFound page

---
+++ my-react-app/front-end/src/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './404.scss';
+
+function NotFound() {
+    return (
+        <div className="NotFound">
+            <div class="error">404</div>
+            <br /><br />
+            <span class="info">File not found</span>
+            <img src="http://images2.layoutsparks.com/1/160030/too-much-tv-static.gif" class="static" />
+        </div>
+    )
+}
+
+export default NotFound;
