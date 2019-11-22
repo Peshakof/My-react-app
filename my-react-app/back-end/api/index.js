ef0d3e552ef4df7767c9309a65d6d4ed65888000
Peshakof <nfs2000@abv.bg>
2019-11-22 17:17:00 +0200

    Updated architecture

--- my-react-app/back-end/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const userRouter = require('./user');
-const expenseRouter = require('./expense-service');
-
-router.use('/expense', expenseRouter);
-router.use('/user', userRouter)
-
-module.exports = router;
--- my-react-app/back-end/models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./user');
-const Expense = require('./expense');
-const Income = require('./income');
-
-module.exports = {
-    User,
-    Expense,
-    Income
-};
