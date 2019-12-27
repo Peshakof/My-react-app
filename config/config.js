392347181c5e5349e78530a642b4f8c7fcbcc25b
Peshakof <nfs2000@abv.bg>
2019-12-27 14:29:14 +0200

    msg

--- config/config.js
+++ config/config.js
@@ -1,12 +1,16 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 
 const config = {
-    development: {
-        port: process.env.PORT || 8080,
-        dbURL: 'mongodb://localhost:27017/money-tracker',
-        authCookieName: 'x-auth-token'
-    },
-    production: {}
+  development: {
+    port: process.env.PORT || 8080,
+    dbURL: 'mongodb://localhost:27017/money-tracker',
+    authCookieName: 'x-auth-token'
+  },
+  production: {
+    port: process.env.PORT || 8080,
+    dbURL: 'mongodb://localhost:27017/money-tracker',
+    authCookieName: 'x-auth-token'
+  }
 };
 
 module.exports = config[env];
